a579f31a663a97d762f97e5a1d646046d61b6a68
Gabriel Juarez De La Cruz <42722962+gajuarezdelac@users.noreply.github.com>
2022-10-20 16:23:01 -0500

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -11,7 +11,7 @@
 		mm = String(today.getMonth() + 1).padStart(2, "0"),
 		yyyy = today.getFullYear(),
 		nextYear = yyyy + 1,
-		dayMonth = "06/30/",
+		dayMonth = "12/01/",
 		birthday = dayMonth + yyyy;
 	
 	today = mm + "/" + dd + "/" + yyyy;
@@ -33,7 +33,7 @@
   
 		  //do something later when date is reached
 		  if (distance < 0) {
-			document.getElementById("headline").innerText = "It's my birthday!";
+			document.getElementById("headline").innerText = "Lo hemos logrado!";
 			document.getElementById("countdown").style.display = "none";
 			document.getElementById("content").style.display = "block";
 			clearInterval(x);
