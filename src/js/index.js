1109683dfa933d144d5d49f74987dd2a25d3f669
Gabriel Juarez de la cruz <gjuarez@agoconsultores.com.mx>
2022-12-29 01:53:13 -0600

    AP

--- src/js/index.js
+++ src/js/index.js
@@ -11,7 +11,7 @@
 		mm = String(today.getMonth() + 1).padStart(2, "0"),
 		yyyy = today.getFullYear(),
 		nextYear = yyyy + 1,
-		dayMonth = "12/01/",
+		dayMonth = "01/01/",
 		birthday = dayMonth + yyyy;
 	
 	today = mm + "/" + dd + "/" + yyyy;
@@ -40,4 +40,7 @@
 		  }
 		  //seconds
 		}, 0)
+
+
+		
 	}());
